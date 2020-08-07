8339607dd6a892932a77c9b88f490aa2b402243d
Adriaan Knapen <hi@aknapen.nl>
2020-08-07 10:43:20 +0200

    refactor: extracts workload logic to application configuration
    Allows having different workload testing logic depending on the application under test.

--- run.py
+++ run.py
@@ -5,24 +5,41 @@ import time
 from contextlib import contextmanager
 from typing import Iterator, TypedDict, Optional, Union, Iterable
 
-APPLICATIONS = {
-    "postgresql-ha": {
-        "chart": "bitnami/postgresql-ha",
-        "version": "3.2.7",
-        "namespace": "postgresql-ha",
-        "values": "postgresql-ha/values.yaml",
-        "repo_name": "bitnami",
-        "repo_url": "https://charts.bitnami.com/bitnami",
-    },
-    "redis-cluster": {
-        "chart": "bitnami/redis-cluster",
-        "version": "3.1.10",
-        "namespace": "redis-cluster",
-        "values": "redis-cluster/values.yaml",
-        "repo_name": "bitnami",
-        "repo_url": "https://charts.bitnami.com/bitnami",
+with open("load.sql") as load_sql:
+    APPLICATIONS = {
+        "postgresql-ha": {
+            "chart": "bitnami/postgresql-ha",
+            "version": "3.2.7",
+            "namespace": "postgresql-ha",
+            "values": "postgresql-ha/values.yaml",
+            "repo_name": "bitnami",
+            "repo_url": "https://charts.bitnami.com/bitnami",
+            "workload": {
+                "image": "bitnami/postgresql:11",
+                "env": {
+                    "PGPASSWORD": "password",
+                    "PGCONNECT_TIMEOUT": "1",
+                },
+                "command": [
+                    'psql',
+                    '-h', 'postgresql-ha-pgpool',  # Set the host
+                    '-p', '5432',  # The port
+                    '-U', 'postgres',  # The user
+                    '-d', 'postgres',  # The database
+                    '-c', load_sql.read(),  # The SQL to run
+                ]
+            }
+        },
+        "redis-cluster": {
+            "chart": "bitnami/redis-cluster",
+            "version": "3.1.10",
+            "namespace": "redis-cluster",
+            "values": "redis-cluster/values.yaml",
+            "repo_name": "bitnami",
+            "repo_url": "https://charts.bitnami.com/bitnami",
+        }
     }
-}
+
 
 
 class Cluster(TypedDict):
@@ -55,6 +72,7 @@ class Arguments(TypedDict):
 def parse_arguments() -> Arguments:
     parser = argparse.ArgumentParser()
     parser.add_argument('command', type=str, choices=['test', 'deploy', 'destroy', 'all'], default='all')
+    parser.add_argument('application', type=str, choices=APPLICATIONS.keys())
     parser.add_argument('--cluster', type=str,
                         help='The name of the cluster to use, if left empty a temporary cluster will be created.')
     args = parser.parse_args()
@@ -111,16 +129,14 @@ def get_container_statuses() -> Iterable:
                 )
 
 
-def run_test(namespace: str):
+def run_test(namespace: str, workload):
     def execute_workload_test():
-        with open("load.sql", "r") as load_sql:
-            return subprocess.run([
-                'kubectl', 'run', 'postgresql-ha-client', '--rm', '--tty', '-i', '--restart', 'Never',
-                '--namespace', namespace, '--image', 'bitnami/postgresql:11', '--env', 'PGPASSWORD=password',
-                '--env', 'PGCONNECT_TIMEOUT=1', '--command',
-                '--', 'psql', '-h', 'postgresql-ha-pgpool', '-p', '5432', '-U', 'postgres', '-d', 'postgres', '-c',
-                load_sql.read()
-            ])
+        return subprocess.run([
+            'kubectl', 'run', 'workload', '--rm', '--tty', '-i', '--restart', 'Never',
+            '--namespace', namespace, '--image', workload["image"],
+            *[x for name, value in workload["env"].items() for x in ("--env", f"{name}={value}")],
+            '--command', '--', *workload["command"]
+        ])
 
     # Register the starting time
     start_time = time.time()
@@ -198,7 +214,7 @@ def run_command(command: Command, cluster_context: str):
         destroy_deployment(app["namespace"])
         destroy_namespace(app["namespace"])
     elif command == "test":
-        run_test(app["namespace"])
+        run_test(app["namespace"], app["workload"])
     elif command == "all":
         run_command("deploy", cluster_context)
         run_command("test", cluster_context)
